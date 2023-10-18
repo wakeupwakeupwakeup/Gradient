a0029d5ad655f64ae5a425ac736874a43eb3891d
wakeupwakeupwakeup <maslakovsana1999@gmail.com>
2023-10-18 16:42:08 +0300

    Mobile version complete

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,6 +1,7 @@
 import Nav from "@/components/Nav";
 import Image from "next/image";
 import FeatureList from "@/components/FeatureList";
+import Services from "@/components/Services";
 
 export default function Home() {
   return (
@@ -76,6 +77,66 @@ export default function Home() {
        {/* Block #3*/}
        <div>
          <FeatureList />
+       </div>
+       {/* Block #4*/}
+       <div className={"block-p"}>
+           <div>
+               <div>
+                   <span className={"heading-top"}>London 2006</span>
+                   <h2 className={"heading"}>We turn your digital dreams into reality</h2>
+               </div>
+               <div className={"text-xs font-extralight my-20"}>
+                   <p className={"pb-6"}>
+                       We specialize in creating unique and innovative designs that not only capture the essence of your business but also help you achieve your goals. We believe that great design is about creating a seamless user experience that engages and delights your audience, and we work hard to ensure that every design we create meets this high standard.
+                   </p>
+               </div>
+           </div>
+           <div>
+               <Services />
+           </div>
+           <div>
+               <div className={"text-xs font-extralight my-20"}>
+                   <div className={"flex mb-3.5"}>
+                       <Image src={"/star.svg"} alt={"star"} width={30} height={30}/>
+                   </div>
+                   <p className={"pb-6"}>
+                       Influenzilla is a leading design and marketing agency that offers creative solutions to businesses of all sizes. Our team of experts specializes in creating unique and effective designs that help our clients stand out in a crowded market.
+                   </p>
+                   <p>
+                       Our mission is to help our clients achieve their business objectives through innovative and effective design solutions. We believe that great design is more than just aesthetics. It's about creating a seamless user experience that engages and delights your audience.
+                   </p>
+               </div>
+           </div>
+       </div>
+       {/* Block #5*/}
+       <div className={"block-p"}>
+           <div>
+               <div className={"flex flex-col h-[452px] justify-between"}>
+                   <span className={"text-xs font-light gradient-text"}>hi there, let's<br />get started!</span>
+                   <h2 className={"heading"}>Expertise and experience</h2>
+                   <p>
+                       We believe that building long-lasting relationships with our clients is key to achieving success. We are passionate about what we do, and we are committed to delivering exceptional results for every client we work with.
+                   </p>
+               </div>
+           </div>
+           <div>
+               <div className={"text-xs font-extralight my-20"}>
+                   <div className={"flex mb-3.5"}>
+                       <Image src={"/star.svg"} alt={"star"} width={30} height={30}/>
+                       <Image src={"/star.svg"} alt={"star"} width={30} height={30}/>
+                       <Image src={"/star.svg"} alt={"star"} width={30} height={30}/>
+                   </div>
+                   <p className={"pb-6"}>
+                       Our team of talented designers and marketers works closely with each of our clients, taking the time to understand their unique needs and goals, and developing customized solutions that exceed their expectations.
+                   </p>
+                   <p>
+                       We take pride in our ability to create a cohesive brand identity, effective marketing strategies, and user-friendly websites and applications that help our clients achieve success.
+                   </p>
+               </div>
+           </div>
+           <div>
+               <Nav />
+           </div>
        </div>
    </div>
   )
---
+++ components/Services.tsx
@@ -0,0 +1,34 @@
+import services from '../public/page_data/services_list.json'
+import Image from 'next/image'
+
+function Spheres(index: number) {
+    const spheres = []
+    for (let i = 0; i < index+1; i++) {
+        spheres.push(
+                <Image className={`${i > 0 ? "-ml-2" : ""}`} src={`/sphere_${i+1}.svg`} alt={`Sphere image`} width={48} height={48} key={i} />
+        )
+    }
+    return spheres
+}
+
+export default function Services() {
+    return (
+        <div className={"flex flex-col border-2 border-white"}>
+            {
+                services.map((service, index) => {
+                    return (
+                        <div>
+                            <div className={"flex border-y-2 border-white px-12 py-5"}>
+                                {Spheres(index)}
+                            </div>
+                            <div className={"px-12 py-8"}>
+                                <h3 className={"heading mb-4"}>{service.title}</h3>
+                                <p className={"text-xs"}>{service.description}</p>
+                            </div>
+                        </div>
+                    )
+                })
+            }
+        </div>
+    )
+}
